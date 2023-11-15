b59c006133154e0142c94ee09b1e512e815d8b62
ReynerWL <reynerwl2018@gmail.com>
2023-11-15 17:04:55 +0700

    feat: Review CRUD

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -3,9 +3,11 @@ import { ReviewController } from './review.controller';
 import { ReviewService } from './review.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Review } from './entities/review.entity';
+import { UsersService } from '#/users/users.service';
+import { UsersModule } from '#/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review])],
+  imports: [TypeOrmModule.forFeature([Review]), UsersModule],
   controllers: [ReviewController],
   providers: [ReviewService]
 })
